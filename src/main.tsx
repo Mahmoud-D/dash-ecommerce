0856fc217a4ef9d5fb39055b884710e25754c116
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-03-08 22:24:11 +0200

    ADD provider store  wraper

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,14 @@ import ReactDOM from 'react-dom/client'
 
 import AppRouter from '@routes/AppRouter'
 
+import store from '@store/index'
+
+import { Provider } from 'react-redux'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<AppRouter />)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+)
