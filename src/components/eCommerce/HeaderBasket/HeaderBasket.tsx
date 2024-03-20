6ea847159805b4c7cf1b6c40ab4d8367005fe880
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-03-20 23:19:26 +0200

    ADD total Quantity functionality

--- src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
+++ src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
@@ -1,12 +1,20 @@
 import Logo from '../../../assets/svg/cart.svg?react'
+import { useAppSelector } from '@store/hooks'
 import styles from './styles.module.css'
 const { basketContainer, basketQuantity } = styles
 
 const HeaderBasket = () => {
+  const cartItems = useAppSelector((state) => state.cart.items)
+  console.log(cartItems)
+  const totalQuantity = Object.values(cartItems).reduce(
+    (acc, item) => acc + item,
+    0
+  )
+  console.log(totalQuantity)
   return (
     <div className={basketContainer}>
       <Logo title="basket icon" />
-      <div className={basketQuantity}>0</div>
+      <div className={basketQuantity}>{totalQuantity}</div>
     </div>
   )
 }
