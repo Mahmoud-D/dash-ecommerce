5e5ac2908f760e7736a354f3a3bf5d2a6aada43f
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-03-21 23:20:24 +0200

    ADD getCartTotalQuantitySelector

--- src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
+++ src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
@@ -1,15 +1,12 @@
-import Logo from '../../../assets/svg/cart.svg?react'
 import { useAppSelector } from '@store/hooks'
+import Logo from '../../../assets/svg/cart.svg?react'
+import { getCartTotalQuantitySelector } from '../../../store/selectors/index'
+
 import styles from './styles.module.css'
 const { basketContainer, basketQuantity } = styles
 
 const HeaderBasket = () => {
-  const cartItems = useAppSelector((state) => state.cart.items)
-  const totalQuantity = Object.values(cartItems).reduce(
-    (acc, item) => acc + item,
-    0
-  )
-
+  const totalQuantity = useAppSelector(getCartTotalQuantitySelector)
   return (
     <div className={basketContainer}>
       <Logo title="basket icon" />
