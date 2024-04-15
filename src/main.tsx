07a8b124c71e3c392917b487dc2658de02e51eb2
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-04-15 22:40:10 +0200

    Wrap App with persistor

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import ReactDOM from 'react-dom/client'
 
 import AppRouter from '@routes/AppRouter'
 
-import store from '@store/index'
+import { store, persistor } from '@store/index'
 
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <AppRouter />
+    <PersistGate loading={null} persistor={persistor}>
+      <AppRouter />
+    </PersistGate>
   </Provider>
 )
